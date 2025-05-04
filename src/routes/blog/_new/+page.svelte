e191cca733c82293abf3bf30753039f156406e69
Thanish Nizam <mnmtanish@gmail.com>
2025-05-04 21:46:17 +0200

    feat: keep new post on local storage

--- src/routes/blog/_new/+page.svelte
+++ src/routes/blog/_new/+page.svelte
@@ -1,10 +1,36 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
 	import { applyAction } from '$app/forms';
 	import { goto, invalidateAll } from '$app/navigation';
 	import type { NewPost } from '$lib/types';
 	import { PostEditor } from '$lib/ui';
 
+	const LS_KEY = 'blog:post-editor-data';
+
 	export let form;
+
+	// Local state for form
+	let post: NewPost = {
+		heading: 'Post Title',
+		slug: '',
+		tags: [],
+		summary: '',
+		content: {}
+	};
+
+	onMount(() => {
+		if (typeof window !== 'undefined') {
+			const savedData = localStorage.getItem(LS_KEY);
+			if (savedData) {
+				post = JSON.parse(savedData);
+			}
+		}
+	});
+
+	function handleChange(_post: NewPost) {
+		const savedData = JSON.stringify(_post);
+		localStorage.setItem(LS_KEY, savedData);
+	}
 
 	async function handleSubmit(formData: NewPost) {
 		const body = new FormData();
@@ -13,11 +39,10 @@
 		body.append('summary', formData.summary);
 		body.append('content', JSON.stringify(formData.content));
 		body.append('tags', JSON.stringify(formData.tags));
-
 		const res = await fetch('?/insert', { method: 'POST', body });
 		await applyAction(await res.json());
 		await invalidateAll();
-
+		localStorage.removeItem(LS_KEY);
 		goto(`/blog/${formData.slug}`);
 	}
 
@@ -33,5 +58,5 @@
 		</div>
 	{/if}
 
-	<PostEditor onSubmit={handleSubmit} onCancel={handleCancel} />
+	<PostEditor bind:post onChange={handleChange} onSubmit={handleSubmit} onCancel={handleCancel} />
 </div>
