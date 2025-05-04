2ae0b7d102b24269d09baa3d82d60047e1dedcd1
Thanish Nizam <mnmtanish@gmail.com>
2025-05-04 18:41:45 +0200

    feat: add post viewer component

---
+++ src/lib/ui/PostViewer.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import type { Post } from '$lib/types';
+	import TextEditor from './TextEditor.svelte';
+
+	export let post: Post;
+</script>
+
+<main>
+	<h1 class="h-24 w-full p-4 text-6xl font-medium">{post.heading}</h1>
+
+	<div class="form-control w-full px-4">
+		<TextEditor data={post.content} help="Post content" className="text-xl" readOnly={true} />
+	</div>
+</main>
--- src/lib/ui/index.ts
+++ src/lib/ui/index.ts
@@ -1,1 +1,2 @@
 export { default as PostEditor } from './PostEditor.svelte';
+export { default as PostViewer } from './PostViewer.svelte';
