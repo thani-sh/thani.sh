5f9d862c995e1c798f1337dbd9bca9f3f44f8beb
Thanish Nizam <mnmtanish@gmail.com>
2025-05-17 22:45:12 +0200

    feat: setup syntax highliting

---
+++ src/routes/blog/(layouts)/blog.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	export let title: string;
+	export let description: string;
+	export let date: string;
+	export let tags: string[];
+
+	const props = {
+		title,
+		description,
+		date,
+		tags
+	};
+</script>
+
+<slot />
+<br /><br />
+<pre>{JSON.stringify(props, null, 2)}</pre>
--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,37 @@
-import { mdsvex } from 'mdsvex';
+import { mdsvex, escapeSvelte } from 'mdsvex';
 import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import rehypeSlug from 'rehype-slug';
+import rehypePrettyCode from 'rehype-pretty-code';
+import { createHighlighter } from 'shiki';
+
+function getAbsolutePath(relativePath) {
+	const url = new URL(relativePath, import.meta.url);
+	return url.pathname;
+}
+
+const theme = 'github-dark';
+const highlighter = await createHighlighter({
+	themes: [theme],
+	langs: ['javascript', 'typescript', 'go', 'bash', 'json', 'html', 'markdown']
+});
 
 const config = {
 	preprocess: [
 		vitePreprocess(),
 		mdsvex({
-			extensions: ['.svx']
+			extensions: ['.svx'],
+			rehypePlugins: [rehypeSlug, rehypeAutolinkHeadings, rehypePrettyCode],
+			highlight: {
+				highlighter: async (code, lang = 'text') => {
+					const html = escapeSvelte(highlighter.codeToHtml(code, { lang, theme }));
+					return `{@html \`${html}\` }`;
+				}
+			},
+			layout: {
+				blog: getAbsolutePath('./src/routes/blog/(layouts)/blog.svelte')
+			}
 		})
 	],
 	kit: { adapter: adapter() },
