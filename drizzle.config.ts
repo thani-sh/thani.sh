93f40353a7f8319774e91f61de35573104265054
Thanish Nizam <mnmtanish@gmail.com>
2025-05-17 17:43:59 +0200

    chore: copy static blog posts from old site

--- drizzle.config.ts
+++
@@ -1,9 +0,0 @@
-import 'dotenv/config';
-import { defineConfig } from 'drizzle-kit';
-
-export default defineConfig({
-	out: './.db',
-	schema: ['./src/lib/server/db/post.schema.ts'],
-	dialect: 'sqlite',
-	dbCredentials: { url: process.env.DB_URL! }
-});
