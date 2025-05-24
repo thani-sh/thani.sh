7d36dae94ef3cdf953b7e079b7da0e66a3d58cfc
Thanish <mnmtanish@gmail.com>
2025-05-24 05:07:26 +0200

    feat: add an embed using parcel

--- src/routes/blog/2020-12-13-multiplayer-tetris-game-part-1/components/GameIframe.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	export let src;
-	export let title;
-	export let height = '600px';
-</script>
-
-<div class="my-4 flex border border-gray-400" style="height: {height};">
-	<iframe {title} {src} width="100%" height="100%" scrolling="no"></iframe>
-</div>
